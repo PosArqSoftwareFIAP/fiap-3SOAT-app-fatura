868eebc2e43c73373d599034d4c236f74c199add
WilliamMp <38511966+WilliamMp@users.noreply.github.com>
2024-05-21 21:00:57 -0300

    Up

--- tests/test_app.py
+++ tests/test_app.py
@@ -7,7 +7,7 @@ from flask_testing import TestCase
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from src.app import app
+from app import app
 
 class TestApp(TestCase):
     def create_app(self):
@@ -17,26 +17,26 @@ class TestApp(TestCase):
         return app
 
     def test_consulta_fatura(self):
-        response = self.client.get('src/fatura/consulta_fatura/3')
+        response = self.client.get('/fatura/consulta_fatura/3')
         self.assertEqual(response.status_code, 200)
 
     def test_consulta_todas_faturas(self):
-        response = self.client.get('src/fatura/consulta_all/')
+        response = self.client.get('/fatura/consulta_all/')
         self.assertEqual(response.status_code, 200)
 
 
     def test_update_fatura_pago(self):
-        response = self.client.put('src/fatura/atualiza_fatura_pago/3')
+        response = self.client.put('/fatura/atualiza_fatura_pago/3')
         self.assertEqual(response.status_code, 200)
     
     
     def test_update_fatura_nao_pago(self):
-        response = self.client.put('src/fatura/atualiza_fatura_nao_pago/3')
+        response = self.client.put('/fatura/atualiza_fatura_nao_pago/3')
         self.assertEqual(response.status_code, 200)
 
 
     def test_update_fatura_cancelado(self):
-        response = self.client.put('src/fatura/atualiza_fatura_cancelado/3')
+        response = self.client.put('/fatura/atualiza_fatura_cancelado/3')
         self.assertEqual(response.status_code, 200)
 
 if __name__ == '__main__':
